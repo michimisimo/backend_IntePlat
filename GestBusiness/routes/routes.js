5d5badb8ebcf6c5400519c979f917790441bb1d9
Felipe Urbina <felipeandrursant@gmail.com>
2025-05-29 10:44:30 -0400

    se arregle routes.js en GestBusiness

--- GestBusiness/routes/routes.js
+++ GestBusiness/routes/routes.js
@@ -3,13 +3,9 @@ const router = express.Router();
 
 // Importar y montar rutas específicas
 const productosRoutes = require('./productos.routes.js');
-const clientesRoutes = require('./clientes.routes.js');
-const empleadosRoutes = require('./empleados.routes.js')
-const sucursalesRoutes = require('./sucursales.routes.js')
+const sucursalesRoutes = require('./sucursales.routes.js');
 
 router.use('/productos', productosRoutes);
-router.use('/clientes', clientesRoutes);
-router.use('/empleados', empleadosRoutes);
-router.use('/sucuersales', sucursalesRoutes)
+router.use('/sucuersales', sucursalesRoutes);
 
 module.exports = router;
